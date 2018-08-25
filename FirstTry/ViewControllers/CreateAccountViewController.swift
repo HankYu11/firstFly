af984f5b0366a6b7089c78e879b2965dec94d4fb
Hank <iamhank1120@gmail.com>
2018-08-25 19:13:49 +0800

    first commit

---
+++ FirstTry/ViewControllers/CreateAccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateAccountViewController.swift
+//  FirstTry
+//
+//  Created by 林子翔 on 2018/8/23.
+//  Copyright © 2018年 林子翔. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
